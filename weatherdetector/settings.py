74a116d880f5830a583bca85ab64cdfdce17f7b8
Chaitan Meshram <118098625+chaitanmeshram@users.noreply.github.com>
2024-07-02 12:17:26 +0530

    Update settings.py

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uv8o3!77bp&l*a4$n++41t7!7m)_1f6@zm_2c+uob(aa1-+=zg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
